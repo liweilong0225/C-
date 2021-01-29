d8da7818938c14ee175fa96ef78a4a39c2676c04
liweilong0225 <liweilong0225@163.com>
2021-01-29 12:55:22 +0800

    字符串反转

--- 字符串是否回文/字符串是否回文.c
+++ 字符串是否回文/字符串是否回文.c
@@ -5,7 +5,7 @@ typedef int BOOL;
 #define TRUE 1
 #define FALSE 0
 
-int Palindrome(char *str);
+BOOL Palindrome(char *str);
 int main()
 {
 	char str[]="abcdefedcba";
@@ -21,7 +21,7 @@ int main()
 	return 0;
 }
 
-int Palindrome(char *str)
+BOOL Palindrome(char *str)
 {
 	char *begin = str;
 	char *end = str + strlen(str) - 1;
